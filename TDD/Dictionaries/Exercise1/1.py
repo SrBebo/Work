2573d4ee8d5acb338817e8c0640f74db464f05bf
Fabricio Simbaña <70413460+SrBebo@users.noreply.github.com>
2023-05-15 12:02:21 -0500

    Create 1.py

---
+++ TDD/Dictionaries/Exercise1/1.py
@@ -0,0 +1,9 @@
+import pytest
+from test import get_name_path_file, open_count_frecuency_file
+
+
+
+name_file = input("Enter the name of file: \n")
+get_name_path_file(name_file)
+open_count_frecuency_file(name_file)
+
