ba84d6cdda7e3bcaa7aed46231f69dec89ba480b
Fabricio Simbaña <70413460+SrBebo@users.noreply.github.com>
2023-05-16 19:03:45 -0500

    Update test.py

--- TDD/Dictionaries/Exercise2/test.py
+++ TDD/Dictionaries/Exercise2/test.py
@@ -1,11 +1,21 @@
-from main import validate_option
+from main import convert_value_to_centimeters, convert_value_to_kilometers, convert_value_to_meters
 
 
 
-def test_validate_option():
-    assert validate_option('1') == True
-    assert validate_option('2') == True
-    assert validate_option('3') == True
-    assert validate_option('4') == True
-    assert validate_option('5') == True
-    assert validate_option('6') == True
+def test_convert_value_to_centimeters():
+    value=0
+    result_expected=5
+    value=convert_value_to_centimeters(50)
+    assert value==result_expected
+
+def test_convert_value_to_meters():
+    value=0
+    result_expected=0.5
+    value=convert_value_to_meters(50)
+    assert value==result_expected
+
+def test_convert_value_to_kilometers():
+    value=0
+    result_expected=50*0.000001
+    value=convert_value_to_kilometers(50)
+    assert value==result_expected
